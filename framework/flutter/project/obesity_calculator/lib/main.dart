95600347f1712da5080ebcef7413beb6a21e530e
kopro-ov <koprodev@gmail.com>
2021-08-12 22:40:05 +0900

    Feat 기본 코드 준비
    1. 첫 번째 페이지 StatefulWidget
    2. 두 번째 페이지 StatelessWidget

--- framework/flutter/project/obesity_calculator/lib/main.dart
+++ framework/flutter/project/obesity_calculator/lib/main.dart
@@ -9,62 +9,34 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: '폼 검증 데모',
+      title: ' Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('폼 검증 데모'),
-        ),
-        body: MyCustomForm(),
-      ),
+      home: BmiMain(),
     );
   }
 }
 
-class MyCustomForm extends StatefulWidget {
-  const MyCustomForm({Key? key}) : super(key: key);
+class BmiMain extends StatefulWidget {
+  const BmiMain({Key? key}) : super(key: key);
 
   @override
-  _MyCustomFormState createState() => _MyCustomFormState();
+  _BmiMainState createState() => _BmiMainState();
 }
 
-class _MyCustomFormState extends State<MyCustomForm> {
-  //Form 위젯에 유니크한 키갑을 부여하고 검증시 사용
-  final _formKey = GlobalKey<FormState>();
+class _BmiMainState extends State<BmiMain> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class BmiResult extends StatelessWidget {
+  const BmiResult({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-        key: _formKey,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            TextFormField(
-              validator: (String? value) {
-                //그냥 value.isEmpty하면 > The property 'isEmpty" can't be unconditionally accessed because the receiver can be 'null'
-                if (value!.isEmpty) {
-                  return '글자를 입력하세요';
-                }
-                return null;
-              },
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 16.0),
-              child: ElevatedButton(
-                onPressed: () {
-                  //폼을 검증하여 통과하면 true 실패하면 false
-                  if (_formKey.currentState!.validate()) {
-                    //검증이 통과하면 스낵바 표시
-                    ScaffoldMessenger.of(context)
-                        .showSnackBar(SnackBar(content: Text('검증 완료')));
-                  }
-                },
-                child: Text('검증'),
-              ),
-            )
-          ],
-        ));
+    return Container();
   }
 }
