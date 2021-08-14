1be317dd262895ad57166df07eb90ec0c9dcde3a
kopro-ov <koprodev@gmail.com>
2021-08-14 21:52:07 +0900

    Feat Bmi계산하여 결과 표시
    1. _calcBmi 메서드 작성 및 계산된 bmi값은 _calcBmi()메서드로 전달
    2. bmi값에 따라 아이콘 표시를 위한 _buildIcon 작업

--- framework/flutter/project/obesity_calculator/lib/main.dart
+++ framework/flutter/project/obesity_calculator/lib/main.dart
@@ -110,6 +110,7 @@ class _BmiMainState extends State<BmiMain> {
 }
 
 class BmiResult extends StatelessWidget {
+
   final double height; //키
   final double weight; //몸무게
 
@@ -117,6 +118,10 @@ class BmiResult extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    final bmi = weight/ ((height/100) *(height / 100));
+    print('bmi: $bmi');
+
     return Scaffold(
       appBar: AppBar(title: Text('비만도 계산기')),
       body: Center(
@@ -124,20 +129,56 @@ class BmiResult extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              '정상',
+              _calcBmi(bmi),
               style: TextStyle(fontSize: 36),
             ),
             SizedBox(
               height: 16,
             ),
-            Icon(
-              Icons.sentiment_satisfied,
-              color: Colors.green,
-              size: 100,
-            ),
+            _buildIcon(bmi) //계산 결과에 따른 아이콘
           ],
         ),
       ),
     );
   }
+
+  //BMI값에 따라 결과 표시
+  String _calcBmi(double bmi) {
+    var result = '저체중';
+    if (bmi >= 35) {
+      result = '고도 비만';
+    } else if (bmi >= 30) {
+      result = '2단계 비만';
+    } else if (bmi >= 25) {
+      result = '1단계 비만';
+    } else if (bmi >= 23) {
+      result = '과체중';
+    } else if (bmi >= 18.5) {
+      result = '정상';
+    }
+    return result;
+  }
+
+  Widget _buildIcon(double bmi) {
+    if (bmi >= 23) {
+      return Icon(
+        Icons.sentiment_very_dissatisfied,
+        color: Colors.red,
+        size: 100,
+      );
+    } else if (bmi >= 18.5) {
+      return Icon(
+        Icons.sentiment_satisfied,
+        color: Colors.green,
+        size: 100,
+      );      
+    } else {
+      return Icon(
+        Icons.sentiment_dissatisfied,
+        color: Colors.orange,
+        size: 100,
+      );      
+    }
+  }
+
 }
