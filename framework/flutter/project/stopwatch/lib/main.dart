486b537193019cfb328e37f97b99012d9db048c8
kopro-ov <koprodev@gmail.com>
2021-08-17 21:59:48 +0900

    Feat 타이머 구현 및 랩타임 기록하기
    1. Timer클래스와 필요한 변수 준비
    2. 시작/일시정지 상태 및 동작 로직
    3. 초기화 버튼
    4. 랩타임 기록 및 표시

--- framework/flutter/project/stopwatch/lib/main.dart
+++ framework/flutter/project/stopwatch/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 void main() => runApp(MyApp());
 
@@ -23,6 +24,13 @@ class StopWatchPage extends StatefulWidget {
 }
 
 class _StopWatchPageState extends State<StopWatchPage> {
+  late Timer _timer; //타이머
+
+  var _time = 0; //0.01초마다 1씩 증가시킬 정수형 변수
+  var _isRunning = false; //현재 시작 상태를 나타낼 불리언 변수
+
+  List<String> _lapTimes = []; //랩타임에 표시할 시간을 저장할 리스트
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,13 +47,16 @@ class _StopWatchPageState extends State<StopWatchPage> {
         onPressed: () => setState(() {
           _clickButton();
         }),
-        child: Icon(Icons.play_arrow),
+        child: _isRunning ? Icon(Icons.pause) : Icon(Icons.play_arrow),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 
   Widget _buildBody() {
+    var sec = _time ~/ 100;
+    var hundredth = '${_time % 100}'.padLeft(2, '0'); //1/100초
+
     return Center(
       child: Padding(
         padding: const EdgeInsets.only(top: 30),
@@ -59,10 +70,10 @@ class _StopWatchPageState extends State<StopWatchPage> {
                   crossAxisAlignment: CrossAxisAlignment.end,
                   children: <Widget>[
                     Text(
-                      '0',
+                      '$sec',
                       style: TextStyle(fontSize: 50.0),
                     ),
-                    Text('0'),
+                    Text('$hundredth'),
                   ],
                 ),
                 Container(
@@ -70,7 +81,7 @@ class _StopWatchPageState extends State<StopWatchPage> {
                   width: 100,
                   height: 200,
                   child: ListView(
-                    children: <Widget>[],
+                    children: _lapTimes.map((time) => Text(time)).toList(),
                   ),
                 )
               ],
@@ -81,7 +92,7 @@ class _StopWatchPageState extends State<StopWatchPage> {
               child: FloatingActionButton(
                 //왼쪽 아래에 위치한 초기화 버튼
                 backgroundColor: Colors.deepOrange,
-                onPressed: () {},
+                onPressed: _reset,
                 child: Icon(Icons.rotate_left),
               ),
             ),
@@ -90,7 +101,11 @@ class _StopWatchPageState extends State<StopWatchPage> {
               bottom: 10,
               child: ElevatedButton(
                 //오른쪽 아래에 위치한 랩타임 버튼
-                onPressed: () {},
+                onPressed: () {
+                  setState(() {
+                    _recordLapTime('$sec.$hundredth');
+                  });
+                },
                 child: Text('랩타임'),
               ),
             )
@@ -100,5 +115,45 @@ class _StopWatchPageState extends State<StopWatchPage> {
     );
   }
 
-  void _clickButton() {}
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
+
+  void _clickButton() {
+    _isRunning = !_isRunning;
+
+    if (_isRunning) {
+      start();
+    } else {
+      _pause();
+    }
+  }
+
+  void start() {
+    _timer = Timer.periodic(Duration(milliseconds: 10), (timer) {
+      setState(() {
+        _time++;
+      });
+    });
+  }
+
+  void _pause() {
+    _timer.cancel();
+  }
+
+  void _reset() {
+    setState(() {
+      _isRunning = false;
+      _timer.cancel();
+      _lapTimes.clear();
+      _time = 0;
+    });
+  }
+
+  //랩타임 기록
+  void _recordLapTime(String time) {
+    _lapTimes.insert(0, '${_lapTimes.length + 1}등 $time');
+  }
 }
