551efc3ecfb743a94fcb9c050be10d9a88f2df40
kopro-ov <koprodev@gmail.com>
2021-08-17 21:45:00 +0900

    Feat 타이머 영역 UI 작성
    1. 시간을 표시하는 영역 작업
    2. 랩타임을 표시하는 영역
    3. 왼쪽 아래 위치한 초기화 버튼
    4. 오른쪽 아래에 위치한 랩타임 버튼

--- framework/flutter/project/stopwatch/lib/main.dart
+++ framework/flutter/project/stopwatch/lib/main.dart
@@ -46,7 +46,58 @@ class _StopWatchPageState extends State<StopWatchPage> {
   }
 
   Widget _buildBody() {
-    return Container();
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.only(top: 30),
+        child: Stack(
+          children: <Widget>[
+            Column(
+              children: <Widget>[
+                Row(
+                  //시간을 표시하는 영역
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: <Widget>[
+                    Text(
+                      '0',
+                      style: TextStyle(fontSize: 50.0),
+                    ),
+                    Text('0'),
+                  ],
+                ),
+                Container(
+                  //랩타임을 표시하는 영역
+                  width: 100,
+                  height: 200,
+                  child: ListView(
+                    children: <Widget>[],
+                  ),
+                )
+              ],
+            ),
+            Positioned(
+              left: 10,
+              bottom: 10,
+              child: FloatingActionButton(
+                //왼쪽 아래에 위치한 초기화 버튼
+                backgroundColor: Colors.deepOrange,
+                onPressed: () {},
+                child: Icon(Icons.rotate_left),
+              ),
+            ),
+            Positioned(
+              right: 10,
+              bottom: 10,
+              child: ElevatedButton(
+                //오른쪽 아래에 위치한 랩타임 버튼
+                onPressed: () {},
+                child: Text('랩타임'),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 
   void _clickButton() {}
