d90ecce456438945ae1b932311245c55a70f8b50
kopro-ov <koprodev@gmail.com>
2021-08-18 21:01:15 +0900

    Feat 할 일 입력 받는 위젯 생성
    1. TextField 위젯
    2. 추가 버튼
    3. 할 일 목록을 표시할 ListView

--- framework/flutter/project/todo/lib/main.dart
+++ framework/flutter/project/todo/lib/main.dart
@@ -34,13 +34,49 @@ class TodoListPage extends StatefulWidget {
 }
 
 class _TodoListPageState extends State<TodoListPage> {
+  //할 일 목록을 저장할 리스트
+  final _items = <Todo>[];
+
+  //할 일 문자열 조작을 위한 컨트롤러
+  var _todoController = TextEditingController();
+
+  @override
+  void dispose() {
+    _todoController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('남은 할 일'),
       ),
-      body: Container(),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: TextField(
+                    controller: _todoController,
+                  ),
+                ),
+                ElevatedButton(
+                  child: Text('추가'),
+                  onPressed: () {},
+                ),
+              ],
+            ),
+            Expanded(
+              child: ListView(
+                children: <Widget>[],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
