d27a6632caabdd797f753e4acf7793118ea57f6a
kopro-ov <koprodev@gmail.com>
2022-04-08 21:18:00 +0900

    네 값 최댓값, 세 값 최솟값, 네 값 최솟값

--- books/자료구조와-함께-배우는-알고리즘-입문/src/chap01/Max3.java
+++ books/자료구조와-함께-배우는-알고리즘-입문/src/chap01/Max3.java
@@ -6,11 +6,50 @@ public class Max3 {
 
     static int max3(int a, int b, int c) {
         int max = a;
+
         if(b > max)
             max = b;
         if(c > max)
             max = c;
+
         return max;
+    }
+
+    static int max4(int a, int b, int c, int d) {
+        int max = a;
+
+        if(b > max)
+            max = b;
+        if(c > max)
+            max = c;
+        if(d > max)
+            max = d;
+
+        return max;
+    }
+
+    static int min3(int a, int b, int c) {
+        int min = a;
+
+        if(b < min)
+            min = b;
+        if(c < min)
+            min = c;
+
+        return min;
+    }
+
+    static int min4(int a, int b, int c, int d) {
+        int min = a;
+
+        if(b < min)
+            min = b;
+        if(c < min)
+            min = c;
+        if(d < min)
+            min = d;
+
+        return min;
     }
 
     public static void main(String[] args) {
@@ -27,7 +66,7 @@ public class Max3 {
         System.out.println("c의 값 : ");
         int c = stdIn.nextInt();
 
-        System.out.println(max3(a, b, c));
+        System.out.println(min3(a, b, c));
 
     }
 
