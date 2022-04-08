2e9386e5acf954faedf6e8627347cae0c4c2b289
kopro-ov <koprodev@gmail.com>
2022-04-08 21:13:18 +0900

    최댓값 구하기 메서드 처리

--- books/자료구조와-함께-배우는-알고리즘-입문/src/chap01/Max3.java
+++ books/자료구조와-함께-배우는-알고리즘-입문/src/chap01/Max3.java
@@ -4,6 +4,15 @@ import java.util.Scanner;
 
 public class Max3 {
 
+    static int max3(int a, int b, int c) {
+        int max = a;
+        if(b > max)
+            max = b;
+        if(c > max)
+            max = c;
+        return max;
+    }
+
     public static void main(String[] args) {
 
         Scanner stdIn = new Scanner(System.in);
@@ -18,11 +27,7 @@ public class Max3 {
         System.out.println("c의 값 : ");
         int c = stdIn.nextInt();
 
-        int max = a;
-        if(b > max) max = b;
-        if(c > max) max = c;
-
-        System.out.println("최댓값은 "+ max +"입니다.");
+        System.out.println(max3(a, b, c));
 
     }
 
