4f192831095a8ea38dd743e02479813b5ba2f0fb
kopro-ov <koprodev@gmail.com>
2022-04-08 21:34:39 +0900

    중앙값 구하기

---
+++ books/자료구조와-함께-배우는-알고리즘-입문/src/chap01/Median.java
@@ -0,0 +1,42 @@
+package chap01;
+
+import java.util.Scanner;
+
+public class Median {
+
+    static int med(int a, int b, int c) {
+        if(a>= b)
+            if(b >= c)
+                return b;
+            else if(a <= c)
+                return a;
+            else
+                return c;
+        else if(a > c)
+            return a;
+        else if(b > c)
+            return c;
+        else
+            return b;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner stdIn = new Scanner(System.in);
+
+
+        System.out.println("세 정수의 중앙값을 구합니다.");
+        System.out.println("a의 값 : ");
+        int a = stdIn.nextInt();
+
+        System.out.println("b의 값 : ");
+        int b = stdIn.nextInt();
+
+        System.out.println("c의 값 : ");
+        int c = stdIn.nextInt();
+
+        System.out.println(med(a, b, c));
+
+    }
+
+}
