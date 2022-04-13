49f57b42526b0132fd90d8cf82269e9324babc63
kopro-ov <koprodev@gmail.com>
2022-04-13 22:20:49 +0900

    Dish 도메인 추가

---
+++ books/스프링부트-실전활용-마스터/hacking-spring-boot-ch1-reactive/src/main/java/com/greglturnquist/hackingspringboot/domain/Dish.java
@@ -0,0 +1,34 @@
+package com.greglturnquist.hackingspringboot.domain;
+
+public class Dish {
+
+    private String description;
+    private boolean delivered = false;
+
+    Dish(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public static Dish deliver(Dish dish) {
+        Dish deliveredDish = new Dish(dish.description);
+        deliveredDish.delivered = true;
+        return deliveredDish;
+    }
+
+    public boolean isDelivered() {
+        return delivered;
+    }
+
+    @Override
+    public String toString() {
+        return "Dish{" +
+                "description='" + description + '\'' +
+                ", delivered=" + delivered +
+                '}';
+    }
+
+}
