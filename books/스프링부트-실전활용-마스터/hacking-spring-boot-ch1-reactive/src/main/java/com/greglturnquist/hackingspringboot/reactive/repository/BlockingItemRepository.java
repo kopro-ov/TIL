033a042fa024f9c7771811708944cce72cae5313
kopro-ov <koprodev@gmail.com>
2022-04-18 23:11:13 +0900

    블로킹 리포지토리 사용 가능성을 낮추기 위해 삭제

--- books/스프링부트-실전활용-마스터/hacking-spring-boot-ch1-reactive/src/main/java/com/greglturnquist/hackingspringboot/reactive/repository/BlockingItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.greglturnquist.hackingspringboot.reactive.repository;
-
-import com.greglturnquist.hackingspringboot.reactive.domain.Item;
-import org.springframework.data.repository.CrudRepository;
-
-public interface BlockingItemRepository extends CrudRepository<Item, String> {
-}
