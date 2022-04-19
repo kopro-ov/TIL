61a1ab5f6a7aac062358fa4156837c5fa783f765
kopro-ov <koprodev@gmail.com>
2022-04-19 23:28:34 +0900

    Cart Service 추가 및 장바구니에 상품담기 메소드 추가

---
+++ books/스프링부트-실전활용-마스터/hacking-spring-boot-ch1-reactive/src/main/java/com/greglturnquist/hackingspringboot/reactive/service/CartService.java
@@ -0,0 +1,39 @@
+package com.greglturnquist.hackingspringboot.reactive.service;
+
+import com.greglturnquist.hackingspringboot.reactive.domain.Cart;
+import com.greglturnquist.hackingspringboot.reactive.domain.CartItem;
+import com.greglturnquist.hackingspringboot.reactive.repository.CartRepository;
+import com.greglturnquist.hackingspringboot.reactive.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Service
+@RequiredArgsConstructor
+public class CartService {
+
+    private final ItemRepository itemRepository;
+    private final CartRepository cartRepository;
+
+    Mono<Cart> addToCart(String cartId, String id) {
+        return cartRepository.findById(cartId)
+                .defaultIfEmpty(new Cart(cartId))
+                .flatMap(cart -> cart.getCartItems().stream()
+                        .filter(cartItem -> cartItem.getItem()
+                                .getId().equals(id))
+                        .findAny()
+                        .map(cartItem -> {
+                            cartItem.increment();
+                            return Mono.just(cart);
+                        })
+                        .orElseGet(()->
+                                itemRepository.findById(id)
+                                    .map(CartItem::new)
+                                    .doOnNext(cartITem->
+                                            cart.getCartItems().add(cartITem))
+                                    .map(cartItem -> cart)
+                        ))
+                .flatMap(cartRepository::save);
+    }
+
+}
