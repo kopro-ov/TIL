74c0f07f14b7074cc70a299cbc014e52631a3743
kopro-ov <koprodev@gmail.com>
2022-04-19 22:18:37 +0900

    카트 컨트롤러 생성

--- books/스프링부트-실전활용-마스터/hacking-spring-boot-ch1-reactive/src/main/java/com/greglturnquist/hackingspringboot/reactive/controller/CartController.java
+++ books/스프링부트-실전활용-마스터/hacking-spring-boot-ch1-reactive/src/main/java/com/greglturnquist/hackingspringboot/reactive/controller/CartController.java
@@ -1,22 +1,30 @@
 package com.greglturnquist.hackingspringboot.reactive.controller;
 
 import com.greglturnquist.hackingspringboot.reactive.domain.Cart;
-import com.greglturnquist.hackingspringboot.reactive.domain.Item;
 import com.greglturnquist.hackingspringboot.reactive.repository.CartRepository;
 import com.greglturnquist.hackingspringboot.reactive.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.reactive.result.view.Rendering;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Controller
-public class HomeController {
+@RequiredArgsConstructor
+public class CartController {
 
-    @GetMapping
-    Mono<String> home() {
-        return Mono.just("home");
+    private final ItemRepository itemRepository;
+    private final CartRepository cartRepository;
+
+    @GetMapping("/cart")
+    Mono<Rendering> cart() {
+
+        return Mono.just(Rendering.view("cart")
+                .modelAttribute("items", itemRepository.findAll())
+                .modelAttribute("cart",
+                        cartRepository.findById("My cart")
+                                .defaultIfEmpty(new Cart("My Cart")))
+                .build());
     }
 
 }
