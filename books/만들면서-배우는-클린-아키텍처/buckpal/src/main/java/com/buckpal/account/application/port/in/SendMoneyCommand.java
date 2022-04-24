edf9bdc2f0f89fcf114bd1d964a626de7513e437
kopro-ov <koprodev@gmail.com>
2022-04-24 23:03:13 +0900

    송금을 위한 유효성 검증
    - 필드에 final을 지정해 불변 필드로 만듬
      (생성에 성공하면 상태는 유효하고 이후에 잘못된 상태로 변경할 수 없다는 사실을 보장)

--- books/만들면서-배우는-클린-아키텍처/buckpal/src/main/java/com/buckpal/account/application/port/in/SendMoneyCommand.java
+++ books/만들면서-배우는-클린-아키텍처/buckpal/src/main/java/com/buckpal/account/application/port/in/SendMoneyCommand.java
@@ -1,4 +1,33 @@
 package com.buckpal.account.application.port.in;
 
-public class SendMoneyCommand {
+import com.buckpal.account.domain.Account.AccountId;
+import com.buckpal.account.domain.Money;
+import com.buckpal.common.SelfValidating;
+import lombok.Getter;
+
+import javax.validation.constraints.NotNull;
+
+
+@Getter
+public class SendMoneyCommand extends SelfValidating<SendMoneyCommand> {
+
+    @NotNull
+    private final AccountId sourceAccountId;
+    @NotNull
+    private final AccountId targetAccountId;
+    @NotNull
+    private final Money money;
+
+    public SendMoneyCommand(
+            AccountId sourceAccountId,
+            AccountId targetAccountId,
+            Money money) {
+        this.sourceAccountId = sourceAccountId;
+        this.targetAccountId = targetAccountId;
+        this.money = money;
+        this.validateSelf();
+    }
+
+
+
 }
