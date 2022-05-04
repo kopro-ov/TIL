e4a5c47d9e6bb57137db08e9273a1e2d3e39f7e7
kopro-ov <koprodev@gmail.com>
2022-05-04 22:50:22 +0900

    신체검사 데이터의 알람표 - PhyData Class 생성

---
+++ books/자료구조와-함께-배우는-알고리즘-입문/src/chap02/PhysicalExamination.java
@@ -0,0 +1,19 @@
+package chap02;
+
+public class PhysicalExamination {
+
+    static final int VMAX = 21;
+
+    static class PhysData {
+        String name;
+        int height;
+        double vision;
+
+        PhysData(String name, int height, double vision) {
+            this.name = name;
+            this.height = height;
+            this.vision = vision;
+        }
+    }
+
+}
