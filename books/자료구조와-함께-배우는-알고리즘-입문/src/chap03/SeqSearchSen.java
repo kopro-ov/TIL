6b60e8f3e74a8f1b2e7b88fa451b3482b25fa70a
kopro-ov <koprodev@gmail.com>
2022-05-06 22:50:10 +0900

    선핵 검색(보초법)

--- books/자료구조와-함께-배우는-알고리즘-입문/src/chap03/SeqSearchSen.java
+++ books/자료구조와-함께-배우는-알고리즘-입문/src/chap03/SeqSearchSen.java
@@ -1,5 +1,7 @@
 package chap03;
 
+import java.util.Scanner;
+
 //선행 검색(보초법)
 public class SeqSearchSen {
 
@@ -14,6 +16,30 @@ public class SeqSearchSen {
             i++;
         }
         return i == n ? -1 : i;
+    }
+
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+
+        System.out.print("요솟수 : ");
+        int num = stdIn.nextInt();
+        int[] x = new int[num + 1];         //요솟수 num + 1
+
+        for (int i = 0; i < num; i++) {
+            System.out.print("x["+i+"] : ");
+            x[i] = stdIn.nextInt();
+        }
+
+        System.out.print("검색 할 값 : ");      //키 값을 입력
+        int ky = stdIn.nextInt();
+
+        int idx = seqSearchSen(x, num, ky);
+
+        if (idx == -1)
+            System.out.println("그 값의 요소가 없습니다.");
+        else
+            System.out.println(ky+"은(는) x["+idx+"]에 있습니다.");
+
     }
 
 }
