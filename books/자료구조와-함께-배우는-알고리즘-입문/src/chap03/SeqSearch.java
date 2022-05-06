0e9d2ccb21d6da716f9ee90bf1c9649750799bdb
kopro-ov <koprodev@gmail.com>
2022-05-06 22:36:09 +0900

    while문이 아니라 for문으로 구현하면 보다 짧고 간결해짐

--- books/자료구조와-함께-배우는-알고리즘-입문/src/chap03/SeqSearch.java
+++ books/자료구조와-함께-배우는-알고리즘-입문/src/chap03/SeqSearch.java
@@ -5,16 +5,20 @@ import java.util.Scanner;
 public class SeqSearch {
 
     static int seqSearch(int[] a, int n, int key) {
-        int i = 0;
-
-        while (true) {
-            if (i == n)
-                return -1; //검색 실패
+//        int i = 0;
+//
+//        while (true) {
+//            if (i == n)
+//                return -1; //검색 실패
+//            if (a[i] == key)
+//                return i; //검색 성공! 인덱스를 반환
+//
+//            i++;
+//        }
+        for (int i = 0; i < n; i++)
             if (a[i] == key)
-                return i; //검색 성공! 인덱스를 반환
-
-            i++;
-        }
+                return i;
+        return -1;
     }
 
     public static void main(String[] args) {
