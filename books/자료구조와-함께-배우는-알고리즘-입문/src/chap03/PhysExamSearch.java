1aef9c30e49e2a34aa25b33fdc39812efb125dc6
kopro-ov <koprodev@gmail.com>
2022-05-08 23:16:23 +0900

    오름차순으로 정렬하기 위한 comparator 생성

--- books/자료구조와-함께-배우는-알고리즘-입문/src/chap03/PhysExamSearch.java
+++ books/자료구조와-함께-배우는-알고리즘-입문/src/chap03/PhysExamSearch.java
@@ -29,5 +29,17 @@ public class PhysExamSearch {
         }
     }
 
+    //오름차순으로 정렬하기 위한 comparator
+    public static final Comparator<PhyscData> HEIGHT_ORDER = new HeightOrderComparator();
+
+
+    private static class HeightOrderComparator implements Comparator<PhyscData> {
+
+        @Override
+        public int compare(PhyscData d1, PhyscData d2) {
+            return (d1.height > d2.height) ? 1 : (d1.height < d2.height) ? -1 : 0;
+        }
+    }
+
 
 }
