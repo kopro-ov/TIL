a270f2eec7203e6bb9a802f1eff04c84f5329eeb
kopro-ov <koprodev@gmail.com>
2022-05-08 23:19:30 +0900

    신체검사 데이터 배열에서 이진 검색

--- books/자료구조와-함께-배우는-알고리즘-입문/src/chap03/PhysExamSearch.java
+++ books/자료구조와-함께-배우는-알고리즘-입문/src/chap03/PhysExamSearch.java
@@ -1,5 +1,6 @@
 package chap03;
 
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Scanner;
 
@@ -27,19 +28,43 @@ public class PhysExamSearch {
                     ", vision=" + vision +
                     '}';
         }
-    }
 
-    //오름차순으로 정렬하기 위한 comparator
-    public static final Comparator<PhyscData> HEIGHT_ORDER = new HeightOrderComparator();
+        //오름차순으로 정렬하기 위한 comparator
+        public static final Comparator<PhyscData> HEIGHT_ORDER = new HeightOrderComparator();
 
 
-    private static class HeightOrderComparator implements Comparator<PhyscData> {
+        private static class HeightOrderComparator implements Comparator<PhyscData> {
 
-        @Override
-        public int compare(PhyscData d1, PhyscData d2) {
-            return (d1.height > d2.height) ? 1 : (d1.height < d2.height) ? -1 : 0;
+            @Override
+            public int compare(PhyscData d1, PhyscData d2) {
+                return (d1.height > d2.height) ? 1 : (d1.height < d2.height) ? -1 : 0;
+            }
         }
     }
 
+
+
+    public static void main(String[] args) {
+
+        Scanner stdIn = new Scanner(System.in);
+        PhyscData[] x = {
+                new PhyscData("이나령", 162, 0.3),
+                new PhyscData("유지훈", 162, 0.4),
+                new PhyscData("김한결", 169, 0.8),
+                new PhyscData("홍준기", 171, 1.5),
+        };
+
+        System.out.print("몇 cm인 사람을 찾고 있나요? : ");
+        int height = stdIn.nextInt();
+        int idx = Arrays.binarySearch(x, new PhyscData("", height, 0.0), PhyscData.HEIGHT_ORDER);
+
+        if (idx < 0) {
+            System.out.println("요소가 없습니다.");
+        } else {
+            System.out.println("x["+idx+"]에 있습니다.");
+            System.out.println("찾은 데이터 : "+x[idx]);
+        }
+
+    }
 
 }
