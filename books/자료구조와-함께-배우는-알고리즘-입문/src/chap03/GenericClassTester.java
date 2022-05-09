f3cdd5a112d6eab4eb065a0c2cb856b7ad3fc3dd
kopro-ov <koprodev@gmail.com>
2022-05-09 22:23:02 +0900

    파라미터에 String이나 Integer를 넘길 수 있음

--- books/자료구조와-함께-배우는-알고리즘-입문/src/chap03/GenericClassTester.java
+++ books/자료구조와-함께-배우는-알고리즘-입문/src/chap03/GenericClassTester.java
@@ -13,4 +13,13 @@ public class GenericClassTester {
         }
     }
 
+    public static void main(String[] args) {
+        //다음과 같이 파라미터에 String을 넘길 수도 있고 Integer를 넘길 수 있다.
+        GenericClass<String> s = new GenericClass<>("ABC");
+        GenericClass<Integer> n = new GenericClass<>(15);
+
+        System.out.println(s.getXyz());
+        System.out.println(n.getXyz());
+    }
+
 }
