3c7809ef8e2cba6b34632c4abf612cd1f49ee8ea
kopro-ov <koprodev@gmail.com>
2022-05-10 17:00:15 +0900

    큐를 구현하는 메서드 생성 - 초기화 - 용량 반환 - 데이터 수 반환 - 큐 존재 여부 - 용량 가득찼는지 여부 - 모든 데이터 표시

--- books/자료구조와-함께-배우는-알고리즘-입문/src/chap04/IntAryQueue.java
+++ books/자료구조와-함께-배우는-알고리즘-입문/src/chap04/IntAryQueue.java
@@ -46,4 +46,51 @@ public class IntAryQueue {
         num--;
         return x;
     }
+
+
+    //큐에서 x를 찾아 인덱스(없으면 -1)반환
+    public int indexOf(int x) {
+        for (int i = 0; i < num; i++) {
+            if (que[i] == x)
+                return i;
+        }
+        return -1;
+    }
+
+    //큐 초기화
+    public void clear() {
+        num = 0;
+    }
+
+    //큐의 용량 반환
+    public int capacity() {
+        return max;
+    }
+
+    //큐에 쌓여 있는 데이터 수를 반환
+    public int size() {
+        return num;
+    }
+
+    //큐가 비어 있는가?
+    public boolean isEmpty() {
+        return num <= 0;
+    }
+
+    //큐가 가득 찼는지 검사하는 메서드 isFull
+    public boolean isFull() {
+        return num >= max;
+    }
+
+    //큐 안에 있는 모든 데이터를 표시하는 메서드
+    public void dump() {
+        if (num <= 0) {
+            System.out.println("큐가 비어 있습니다.");
+        } else {
+            for (int i = 0; i < num; i++) {
+                System.out.print(que[i]+ " ");
+            }
+            System.out.println();
+        }
+    }
 }
