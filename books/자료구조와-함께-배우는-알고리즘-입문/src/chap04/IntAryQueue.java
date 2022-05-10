e0728e6dd40e96a10a9c3dd4cf7a91c3bdfb5d96
kopro-ov <koprodev@gmail.com>
2022-05-10 16:49:40 +0900

    큐를 구현하는 클래스 생성

---
+++ books/자료구조와-함께-배우는-알고리즘-입문/src/chap04/IntAryQueue.java
@@ -0,0 +1,26 @@
+package chap04;
+
+public class IntAryQueue {
+
+    private int max;        //큐 용량
+    private int num;        //현재 데이터 수
+    private int[] que;      //큐 본체
+
+    public class EmptyIntAryQueueException extends RuntimeException {
+        public EmptyIntAryQueueException() {}
+    }
+    public class OverflowIntAryQueueException extends RuntimeException {
+        public OverflowIntAryQueueException() {}
+    }
+
+    public IntAryQueue(int capacity) {
+        max = capacity;
+        num = 0;
+        try {
+            que = new int[max];
+        } catch (OutOfMemoryError e) {
+            max = 0;
+        }
+    }
+
+}
