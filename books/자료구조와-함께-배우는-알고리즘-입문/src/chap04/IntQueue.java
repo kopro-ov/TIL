a3ae6e366df21b33c25146c85e9ef7afb3363aed
kopro-ov <koprodev@gmail.com>
2022-05-11 22:57:17 +0900

    인큐 메서드 - 인큐에 성공하면 인큐한 값을 그대로 반환 - 큐가 가득차면 예외를 던짐 - rear값을 1만큼 증가했을 때 큐의 최대 용량 값과 같은 경우 rear를 배열의 처음인 0으로 변경

--- books/자료구조와-함께-배우는-알고리즘-입문/src/chap04/IntQueue.java
+++ books/자료구조와-함께-배우는-알고리즘-입문/src/chap04/IntQueue.java
@@ -29,4 +29,15 @@ public class IntQueue {
         }
     }
 
+    //큐에 데이터를 인큐
+    public int enque(int x) throws OverflowIntQueueException {
+        if (num >= max)
+            throw new OverflowIntQueueException();
+        que[rear++] = x;
+        num++;
+        if (rear == max)
+            rear = 0;
+        return x;
+    }
+
 }
