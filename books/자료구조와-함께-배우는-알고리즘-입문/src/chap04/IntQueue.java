8b27e569c1926e612e486673807a80560ed857c4
kopro-ov <koprodev@gmail.com>
2022-05-11 23:01:05 +0900

    디큐 메서드 생성 - 큐가 비어 있어 디큐할 수 없는 경우 예외를 던짐 - front값을 1만큼 증가한 다음 num값을 1만큼 감소 - front값이 max와 같으면 front값을 0으로 처리

--- books/자료구조와-함께-배우는-알고리즘-입문/src/chap04/IntQueue.java
+++ books/자료구조와-함께-배우는-알고리즘-입문/src/chap04/IntQueue.java
@@ -40,4 +40,15 @@ public class IntQueue {
         return x;
     }
 
+    //큐에서 데이터를 디큐
+    public int deque() throws EmptyIntQueueException {
+        if (num <= 0)
+            throw new EmptyIntQueueException();
+        int x = que[front++];
+        num--;
+        if (front == max)
+            front = 0;
+        return x;
+    }
+
 }
