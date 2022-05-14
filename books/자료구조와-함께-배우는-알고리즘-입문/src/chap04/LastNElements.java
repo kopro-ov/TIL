aba96e755c5b91f1228fa7b6d61c76361a506d40
kopro-ov <koprodev@gmail.com>
2022-05-14 22:31:49 +0900

    오래된 데이터를 버리는 링버퍼 - 변수 정의

---
+++ books/자료구조와-함께-배우는-알고리즘-입문/src/chap04/LastNElements.java
@@ -0,0 +1,20 @@
+package chap04;
+
+import java.util.Scanner;
+
+//원하는 개수만큼 값을 입력 받아 요솟수가 N인 배열에 마지막 N개를 저장
+public class LastNElements {
+
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+        final int N = 10; //입력 받을 값을 저장
+        int cnt = 0;      //입력 받을 개수
+        int retry;        //다시 한 번?
+
+        System.out.println("정수를 입력하세요.");
+
+
+
+    }
+
+}
