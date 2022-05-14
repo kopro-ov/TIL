79bb04ec91d565c1e37e1e72719edc4d1b6130ae
kopro-ov <koprodev@gmail.com>
2022-05-14 22:33:45 +0900

    오래된 데이터를 버리는 링버퍼 - 정수 추가

--- books/자료구조와-함께-배우는-알고리즘-입문/src/chap04/LastNElements.java
+++ books/자료구조와-함께-배우는-알고리즘-입문/src/chap04/LastNElements.java
@@ -7,13 +7,19 @@ public class LastNElements {
 
     public static void main(String[] args) {
         Scanner stdIn = new Scanner(System.in);
-        final int N = 10; //입력 받을 값을 저장
-        int cnt = 0;      //입력 받을 개수
-        int retry;        //다시 한 번?
+        final int N = 10;
+        int[] a = new int[N];  //입력 받을 값을 저장
+        int cnt = 0;           //입력 받을 개수
+        int retry;             //다시 한 번?
 
         System.out.println("정수를 입력하세요.");
 
-
+        do {
+            System.out.printf("%d번째 정수 : ",cnt+1);
+            a[cnt++ % N] = stdIn.nextInt();
+            System.out.print("계속 할까요? (예.1 / 아니오 0.");
+            retry = stdIn.nextInt();
+        } while (retry == 1);
 
     }
 
