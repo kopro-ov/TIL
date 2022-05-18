d82d61b42ea314d288ab4f7304de71bcb89db94f
kopro-ov <koprodev@gmail.com>
2022-05-18 15:34:42 +0900

    꼬리 재귀를 제거하는 메소드

---
+++ books/자료구조와-함께-배우는-알고리즘-입문/src/chap05/RecurX1.java
@@ -0,0 +1,13 @@
+package chap05;
+
+public class RecurX1 {
+
+    //꼬리 재귀를 제거했다.
+    static void recur(int n) {
+        while (n > 0) {
+            recur(n - 1);
+            System.out.println(n);
+            n = n -2;
+        }
+    }
+}
