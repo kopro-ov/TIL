17f2c8f85365644cd192d1eaecda9dd6e3d329a4
kopro-ov <koprodev@gmail.com>
2022-05-18 23:37:22 +0900

    Stack 위젯을 이용하여 Image를 순서대로 쌓기

--- books/쉽고-빠른-플러터-앱개발/flutter_basic_01/lib/main.dart
+++ books/쉽고-빠른-플러터-앱개발/flutter_basic_01/lib/main.dart
@@ -51,11 +51,19 @@ class MyHomePage extends StatelessWidget {
         ),
       ),
       body: Center(
-        child: ListView.builder(
-          itemCount: 50,
-          itemBuilder: (BuildContext buildContext, int index) {
-            return Text('$index' + ' Text');
-          },
+        child: Stack(
+          children: [
+            Image.asset(
+              'images/logo.png',
+              width: 500,
+              height: 500,
+            ),
+            Image.network(
+              'https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMjAyMjhfMTIx%2FMDAxNjQ2MDQ5Nzc0MzYx.5-e6Mv-NIxtatcBoAtVGuR-_RwoksYiN2jXEMxTlA1og.fyUs_n_nw7aFu97y2wgcbhYt7PF_KVp8cG8Hw30F0CEg.JPEG.ksy9823%2F%25C1%25D2%25B8%25A3%25B5%25F0-2.jpg&type=a340',
+              width: 200,
+              height: 200,
+            ),
+          ],
         ),
       ),
     );
