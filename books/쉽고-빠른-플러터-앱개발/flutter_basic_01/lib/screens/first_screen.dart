3d72b53aff4471d496dad6f43c96cb6b25591790
kopro-ov <koprodev@gmail.com>
2022-05-19 17:00:14 +0900

    frist screen 생성

---
+++ books/쉽고-빠른-플러터-앱개발/flutter_basic_01/lib/screens/first_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class FirstScreen extends StatelessWidget {
+  const FirstScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            const Text('1번째 화면'),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text("2번째 화면으로 이동"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
