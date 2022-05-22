07cf2a74304609127e155264e4b0e99db028b1bd
kopro-ov <koprodev@gmail.com>
2022-05-22 16:41:17 +0900

    기능 추가

--- books/쉽고-빠른-플러터-앱개발/flutter_basic_01/lib/screens/calculator_screen.dart
+++ books/쉽고-빠른-플러터-앱개발/flutter_basic_01/lib/screens/calculator_screen.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 class CalculatorScreen extends StatefulWidget {
   const CalculatorScreen({Key? key}) : super(key: key);
@@ -10,13 +8,80 @@ class CalculatorScreen extends StatefulWidget {
 }
 
 class _CalculatorScreenState extends State<CalculatorScreen> {
-  late int result;
-  late int num;
+  late String _output;
+  int _num1 = 0;
+  int _num2 = 0;
+
   @override
   void initState() {
     super.initState();
-    result = 0;
-    num = 0;
+    _output = "0";
+  }
+
+  void addNumbers(int n) {
+    setState(() {
+      if (_num1 == 0) {
+        _num2 = n;
+      } else {
+        _num2 = int.parse('$_num2$n');
+      }
+      _output = _num2.toString();
+    });
+  }
+
+  void operand(String o) {
+    switch (o) {
+      case 'AC':
+        setState(() {
+          _num1 = 0;
+          _num2 = 0;
+          _output = "0";
+        });
+        break;
+      case "+/-":
+        setState(() {
+          _num2 = -_num2;
+          _output = _num2.toString();
+        });
+        break;
+      case "-":
+        setState(() {
+          _num1 -= _num2;
+          _num2 = 0;
+          _output = _num1.toString();
+        });
+        break;
+      case "+":
+        setState(() {
+          _num1 += _num2;
+          _num2 = 0;
+          _output = _num1.toString();
+        });
+        break;
+      case "*":
+        setState(() {
+          if (_num1 == 0) _num1 = 1;
+          _num1 *= _num2;
+          _num2 = 0;
+          _output = _num1.toString();
+        });
+        break;
+      case "÷":
+        setState(() {
+          //_num1 /= _num2;
+          _num2 = 0;
+          _output = _num1.toString();
+        });
+        break;
+      case "-/+":
+        setState(() {
+          _num2 = -_num2;
+          _output = _num2.toString();
+        });
+        break;
+      default:
+        break;
+    }
   }
 
   @override
@@ -27,7 +92,7 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
           width: MediaQuery.of(context).size.width,
           padding: EdgeInsets.all(20.0),
           child: Text(
-            '$result',
+            '$_output',
             textAlign: TextAlign.right,
             style: TextStyle(fontSize: 25),
           ),
@@ -35,61 +100,123 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
         Row(
           children: [
             Expanded(
-              child: ElevatedButton(onPressed: () {}, child: Text('AC')),
+              child: ElevatedButton(
+                  onPressed: () {
+                    operand('AC');
+                  },
+                  child: Text('AC')),
             ),
             Expanded(
-              child: ElevatedButton(onPressed: () {}, child: Text('+/-')),
+              child: ElevatedButton(
+                  onPressed: () {
+                    operand('+/-');
+                  },
+                  child: Text('+/-')),
             ),
             Expanded(
-              child: ElevatedButton(onPressed: () {}, child: Text('%')),
+              child: ElevatedButton(
+                  onPressed: () {
+                    operand('%');
+                  },
+                  child: Text('%')),
             ),
             Expanded(
-              child: ElevatedButton(onPressed: () {}, child: Text('÷')),
-            ),
-          ],
-        ),
-        Row(
-          children: [
-            Expanded(
-              child: ElevatedButton(onPressed: () {}, child: Text('7')),
-            ),
-            Expanded(
-              child: ElevatedButton(onPressed: () {}, child: Text('8')),
-            ),
-            Expanded(
-              child: ElevatedButton(onPressed: () {}, child: Text('9')),
-            ),
-            Expanded(
-              child: ElevatedButton(onPressed: () {}, child: Text('X')),
-            ),
-          ],
-        ),
-        Row(
-          children: [
-            Expanded(
-              child: ElevatedButton(onPressed: () {}, child: Text('4')),
-            ),
-            Expanded(
-              child: ElevatedButton(onPressed: () {}, child: Text('5')),
-            ),
-            Expanded(
-              child: ElevatedButton(onPressed: () {}, child: Text('6')),
-            ),
-            Expanded(
-              child: ElevatedButton(onPressed: () {}, child: Text('-')),
+              child: ElevatedButton(
+                  onPressed: () {
+                    operand('÷');
+                  },
+                  child: Text('÷')),
             ),
           ],
         ),
         Row(
           children: [
             Expanded(
-              child: ElevatedButton(onPressed: () {}, child: Text('1')),
+              child: ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      addNumbers(7);
+                    });
+                  },
+                  child: Text('7')),
             ),
             Expanded(
-              child: ElevatedButton(onPressed: () {}, child: Text('2')),
+              child: ElevatedButton(
+                  onPressed: () {
+                    addNumbers(8);
+                  },
+                  child: Text('8')),
             ),
             Expanded(
-              child: ElevatedButton(onPressed: () {}, child: Text('3')),
+              child: ElevatedButton(
+                  onPressed: () {
+                    addNumbers(9);
+                  },
+                  child: Text('9')),
+            ),
+            Expanded(
+              child: ElevatedButton(
+                  onPressed: () {
+                    operand("*");
+                  },
+                  child: Text('*')),
+            ),
+          ],
+        ),
+        Row(
+          children: [
+            Expanded(
+              child: ElevatedButton(
+                  onPressed: () {
+                    addNumbers(4);
+                  },
+                  child: Text('4')),
+            ),
+            Expanded(
+              child: ElevatedButton(
+                  onPressed: () {
+                    addNumbers(5);
+                  },
+                  child: Text('5')),
+            ),
+            Expanded(
+              child: ElevatedButton(
+                  onPressed: () {
+                    addNumbers(6);
+                  },
+                  child: Text('6')),
+            ),
+            Expanded(
+              child: ElevatedButton(
+                  onPressed: () {
+                    operand("-");
+                  },
+                  child: Text('-')),
+            ),
+          ],
+        ),
+        Row(
+          children: [
+            Expanded(
+              child: ElevatedButton(
+                  onPressed: () {
+                    addNumbers(1);
+                  },
+                  child: Text('1')),
+            ),
+            Expanded(
+              child: ElevatedButton(
+                  onPressed: () {
+                    addNumbers(2);
+                  },
+                  child: Text('2')),
+            ),
+            Expanded(
+              child: ElevatedButton(
+                  onPressed: () {
+                    addNumbers(3);
+                  },
+                  child: Text('3')),
             ),
             Expanded(
               child: ElevatedButton(onPressed: () {}, child: Text('4')),
@@ -103,10 +230,18 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
               child: ElevatedButton(onPressed: () {}, child: Text('0')),
             ),
             Expanded(
-              child: ElevatedButton(onPressed: () {}, child: Text('.')),
+              child: ElevatedButton(
+                  onPressed: () {
+                    operand(".");
+                  },
+                  child: Text('.')),
             ),
             Expanded(
-              child: ElevatedButton(onPressed: () {}, child: Text('=')),
+              child: ElevatedButton(
+                  onPressed: () {
+                    operand("=");
+                  },
+                  child: Text('=')),
             ),
           ],
         )
