95034b59602ce503ed3b8483f6cbe9d0c3ce0853
kopro-ov <koprodev@gmail.com>
2022-05-23 23:27:26 +0900

    Builder로 Bloc 객체를 받아와 위젯 형태로 빌드

--- books/쉽고-빠른-플러터-앱개발/flutter_basic_01/lib/main.dart
+++ books/쉽고-빠른-플러터-앱개발/flutter_basic_01/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_basic_01/counter/bloc/counter_bloc.dart';
 import 'package:flutter_basic_01/screens/calculator_screen.dart';
 import 'package:flutter_basic_01/screens/first_screen.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'screens/counter_screen.dart';
 
@@ -42,6 +44,26 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text('계산기')), body: CalculatorScreen());
+      appBar: AppBar(title: Text('계산기')),
+      body: BlocBuilder<CounterBloc, CounterState>(
+        buildWhen: (previous, current) => previous.count != current.count,
+        builder: (context, state) {
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('Counter : ' + state.count.toString()),
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pushNamed('/counter');
+                  },
+                  child: Text('Go to CounterScreen'),
+                )
+              ],
+            ),
+          );
+        },
+      ),
+    );
   }
 }
