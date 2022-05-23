a22b8c433cd2da7b71ab5d0d5560b0ce8a3ead7e
kopro-ov <koprodev@gmail.com>
2022-05-23 23:12:29 +0900

    어떤 이벤트가 입력으로 들어오는지에 따르상태 변화 구현

--- books/쉽고-빠른-플러터-앱개발/flutter_basic_01/lib/counter/bloc/counter_bloc.dart
+++ books/쉽고-빠른-플러터-앱개발/flutter_basic_01/lib/counter/bloc/counter_bloc.dart
@@ -5,9 +5,13 @@ part 'counter_event.dart';
 part 'counter_state.dart';
 
 class CounterBloc extends Bloc<CounterEvent, CounterState> {
-  CounterBloc() : super(CounterInitial()) {
-    on<CounterEvent>((event, emit) {
-      // TODO: implement event handler
+  CounterBloc() : super(CounterInitial(0)) {
+    on<CounterEvent>((event, emit) async* {
+      if (event is CounterIncrement) {
+        yield CounterInitial(state.count + 1);
+      } else if (event is CounterDecrement) {
+        yield CounterInitial(state.count - 1);
+      }
     });
   }
 }
