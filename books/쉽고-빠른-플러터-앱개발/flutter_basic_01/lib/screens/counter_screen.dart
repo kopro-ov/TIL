bf4dd02d513228f8295479dabbeb561a4eaef5ee
kopro-ov <koprodev@gmail.com>
2022-05-23 23:31:56 +0900

    상태의 변화를 바꾸어 Bloc객체로 전달

--- books/쉽고-빠른-플러터-앱개발/flutter_basic_01/lib/screens/counter_screen.dart
+++ books/쉽고-빠른-플러터-앱개발/flutter_basic_01/lib/screens/counter_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_basic_01/counter/bloc/counter_bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CounterScreen extends StatelessWidget {
   const CounterScreen({Key? key}) : super(key: key);
@@ -8,7 +10,32 @@ class CounterScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('?')),
+      body: BlocBuilder<CounterBloc, CounterState>(
+        builder: (context, state) {
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('Counter : ' + state.count.toString()),
+                TextButton(
+                  onPressed: () {
+                    BlocProvider.of<CounterBloc>(context)
+                        .add(CounterIncrement());
+                  },
+                  child: Text('[+] Increment'),
+                ),
+                TextButton(
+                  onPressed: () {
+                    BlocProvider.of<CounterBloc>(context)
+                        .add(CounterDecrement());
+                  },
+                  child: Text('[-] Decrement'),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
     );
   }
 }
