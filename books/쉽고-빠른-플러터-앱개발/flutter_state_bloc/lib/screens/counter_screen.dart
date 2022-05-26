552aa6064b667deeb3378a123a29dc90d880c345
kopro-ov <koprodev@gmail.com>
2022-05-26 22:46:35 +0900

    add CounterScreen
    - 이벤트 발생 시키기 위해 BlocProvider.of(context) 문법 사용

---
+++ books/쉽고-빠른-플러터-앱개발/flutter_state_bloc/lib/screens/counter_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_state_bloc/blocs/counter/bloc/counter_bloc.dart';
+
+class CounterScreen extends StatelessWidget {
+  const CounterScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: BlocBuilder<CounterBloc, CounterState>(
+        builder: (context, state) {
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Counter : ' + state.count.toString(),
+                ),
+                TextButton(
+                  onPressed: () {
+                    BlocProvider.of<CounterBloc>(context)
+                        .add(CounterIncrement());
+                  },
+                  child: Text('[+] Increment'),
+                ),
+                TextButton(
+                  onPressed: () {
+                    BlocProvider.of<CounterBloc>(context)
+                        .add(CounterDecrement());
+                  },
+                  child: Text('[-] Decrement'),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
