a4c836fae850c80e8b0bdf9e1c69496102ffd3ba
kopro-ov <koprodev@gmail.com>
2022-05-27 22:34:20 +0900

    add state pizza loaded

--- books/쉽고-빠른-플러터-앱개발/flutter_state_bloc8/lib/blocs/pizza/bloc/pizza_state.dart
+++ books/쉽고-빠른-플러터-앱개발/flutter_state_bloc8/lib/blocs/pizza/bloc/pizza_state.dart
@@ -2,9 +2,18 @@ part of 'pizza_bloc.dart';
 
 abstract class PizzaState extends Equatable {
   const PizzaState();
-  
+
   @override
   List<Object> get props => [];
 }
 
 class PizzaInitial extends PizzaState {}
+
+class PizzLoaded extends PizzaState {
+  final List<Pizza> pizzas;
+
+  const PizzLoaded({required this.pizzas});
+
+  @override
+  List<Object> get props => [pizzas];
+}
