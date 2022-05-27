b700fd4d34c01fe9302485eadade273da3b11def
kopro-ov <koprodev@gmail.com>
2022-05-27 22:51:40 +0900

    bloc 생성

--- books/쉽고-빠른-플러터-앱개발/flutter_state_bloc8/lib/blocs/pizza/bloc/pizza_bloc.dart
+++ books/쉽고-빠른-플러터-앱개발/flutter_state_bloc8/lib/blocs/pizza/bloc/pizza_bloc.dart
@@ -7,8 +7,31 @@ part 'pizza_state.dart';
 
 class PizzaBloc extends Bloc<PizzaEvent, PizzaState> {
   PizzaBloc() : super(PizzaInitial()) {
-    on<PizzaEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+    on<LoadPizzaCounter>(
+      (event, emit) async {
+        await Future<void>.delayed(const Duration(seconds: 1));
+        emit(const PizzaLoaded(pizzas: <Pizza>[]));
+      },
+    );
+    on<AddPizza>(
+      (event, emit) {
+        if (state is PizzaLoaded) {
+          final state = this.state as PizzaLoaded;
+          emit(
+            PizzaLoaded(pizzas: List.from(state.pizzas)..add(event.pizza)),
+          );
+        }
+      },
+    );
+    on<RemovePizza>(
+      (event, emit) {
+        if (state is PizzaLoaded) {
+          final state = this.state as PizzaLoaded;
+          emit(
+            PizzaLoaded(pizzas: List.from(state.pizzas)..remove(event.pizza)),
+          );
+        }
+      },
+    );
   }
 }
