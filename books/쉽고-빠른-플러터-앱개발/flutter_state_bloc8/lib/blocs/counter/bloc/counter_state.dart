dca1ee1265bedbfb1dd4e28a4d4a1f0416b9673b
kopro-ov <koprodev@gmail.com>
2022-05-28 22:30:15 +0900

    counter state 정의

--- books/쉽고-빠른-플러터-앱개발/flutter_state_bloc8/lib/blocs/counter/bloc/counter_state.dart
+++ books/쉽고-빠른-플러터-앱개발/flutter_state_bloc8/lib/blocs/counter/bloc/counter_state.dart
@@ -1,10 +1,17 @@
 part of 'counter_bloc.dart';
 
 abstract class CounterState extends Equatable {
-  const CounterState();
-  
+  final int count;
+
+  const CounterState(this.count);
+
   @override
   List<Object> get props => [];
 }
 
-class CounterInitial extends CounterState {}
+class CounterInitial extends CounterState {
+  const CounterInitial(int count) : super(count);
+
+  @override
+  String toString() => 'CounterInitial { count: $count }';
+}
