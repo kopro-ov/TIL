77030188893116f600a67e96436522515bebf1e9
kopro-ov <koprodev@gmail.com>
2022-06-16 01:32:04 +0900

    Chore 정적 패터리 메서드 장점
    1. 반환 타입의 하위 타입 객체를 반환할 수 있는 능력
    - 엄청난 유연성
    - 자바 8부턴 인터페이스에도 public 정적 멤버만 허용
    
    2.입력 매개변수에 따라 매번 다른 클래스의 객체를 반환할 수 있다.

--- programming-language/java/project/ex/src/effectiveJava/chapter01/item01/HelloService.java
+++ programming-language/java/project/ex/src/effectiveJava/chapter01/item01/HelloService.java
@@ -3,4 +3,13 @@ package effectiveJava.chapter01.item01;
 public interface HelloService {
 
     String hello();
+
+    static HelloService of(String lang) {
+        if(lang.equals("ko")) {
+            return new KoreanHelloService();
+        } else {
+            return new EnglishHelloService();
+        }
+    }
+
 }
--- programming-language/java/project/ex/src/effectiveJava/chapter01/item01/HelloServiceFactory.java
+++ programming-language/java/project/ex/src/effectiveJava/chapter01/item01/HelloServiceFactory.java
@@ -2,11 +2,8 @@ package effectiveJava.chapter01.item01;
 
 public class HelloServiceFactory {
 
-    public static HelloService of(String lang) {
-        if(lang.equals("ko")) {
-            return new KoreanHelloService();
-        } else {
-            return new EnglishHelloService();
-        }
+    public static void main(String[] args) {
+        HelloService helloService = HelloService.of("ko");
+        System.out.println(helloService.hello());
     }
 }
