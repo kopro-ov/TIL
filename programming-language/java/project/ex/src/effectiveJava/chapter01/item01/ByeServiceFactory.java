816214307fc9b5f7ac66440f0f56d97b675ccc28
kopro-ov <koprodev@gmail.com>
2022-06-17 01:51:38 +0900

    Chore 서비스에 맞게 재정의

--- programming-language/java/project/ex/src/effectiveJava/chapter01/item01/ByeServiceFactory.java
+++ programming-language/java/project/ex/src/effectiveJava/chapter01/item01/ByeServiceFactory.java
@@ -1,5 +1,7 @@
 package effectiveJava.chapter01.item01;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.ServiceLoader;
 
@@ -10,11 +12,14 @@ public class ByeServiceFactory {
 //        public static <S> ServiceLoader<S> load(ModuleLayer layer, Class<S> service) {
 //            return new ServiceLoader<>(Reflection.getCallerClass(), layer, service);
 //        }
-        ServiceLoader<HelloService> loader = ServiceLoader.load(HelloService.class);
-        Optional<HelloService> helloService = loader.findFirst();
-        helloService.ifPresent(h -> {
-            h.hello();
+        ServiceLoader<ByeService> loader = ServiceLoader.load(ByeService.class);
+        Optional<ByeService> byeServiceOptional = loader.findFirst();
+        byeServiceOptional.ifPresent(h -> {
+            h.bye();
         });
 
+        ByeService byeService = new KoreanByeService();
+        List list = new ArrayList<>(); //new도 허용
+        List.of("ttt", "222"); //정적 팩터리 메서드
     }
 }
---
+++ programming-language/java/project/ex/src/effectiveJava/chapter01/item01/KoreanByeService.java
@@ -0,0 +1,8 @@
+package effectiveJava.chapter01.item01;
+
+public class KoreanByeService implements ByeService{
+    @Override
+    public String bye() {
+        return "안녕";
+    }
+}
