b69e61f488474c15cbb895d75d18b016b05bf665
kopro-ov <koprodev@gmail.com>
2022-06-20 00:05:24 +0900

    Chore 싱글턴 문제 예시 소스 추가
    Workflow
    1. 리플렉션 API
    2. 직렬화&역직렬화
    3. 예외 처리

--- programming-language/java/project/ex/src/effectiveJava/chapter01/item03/Elvis.java
+++ programming-language/java/project/ex/src/effectiveJava/chapter01/item03/Elvis.java
@@ -3,7 +3,11 @@ package effectiveJava.chapter01.item03;
 public class Elvis {
 
     public static final Elvis INSTANCE = new Elvis();
-    private Elvis(){}
+    private Elvis()  {
+        if(INSTANCE != null) {
+            //예외 처리
+        }
+    }
 
     public void leaveTheBuilding(){}
 
