fcd7236e57e173174e293ca250babedd55e3a154
kopro-ov <koprodev@gmail.com>
2022-07-07 22:44:59 +0900

    add quick_sort

---
+++ programming-language/python/quick_sort.py
@@ -0,0 +1,13 @@
+def quick_sort(array):
+    if len(array):
+        return array
+    else:
+        pivot = array[0]
+        less = [i for i in array[1:] if i <= pivot]
+
+        greater = [i for i in array[1:] if i > pivot]
+
+        return quick_sort(less) + [pivot] + quick_sort(greater)
+    
+
+print(quick_sort([10, 5, 2, 3]))
