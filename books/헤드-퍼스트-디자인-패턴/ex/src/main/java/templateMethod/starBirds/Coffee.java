bebd3f34dc114f875071cc82e920541293d218ae
kopro-ov <koprodev@gmail.com>
2022-07-19 02:08:55 +0900

    Refactor convert java project to gradle
    자바 프로젝트 관리를 위해 작업 의존성 그래프를 기반으로 하는
    gradle 적용

--- books/헤드-퍼스트-디자인-패턴/ex/src/main/java/templateMethod/starBirds/Coffee.java
+++ books/헤드-퍼스트-디자인-패턴/ex/src/main/java/templateMethod/starBirds/Coffee.java
@@ -1,6 +1,6 @@
-package main.java.templateMethod.starBirds;
+package templateMethod.starBirds;
 
-public class Coffee extends CaffeineBeverage{
+public class Coffee extends CaffeineBeverage {
 
     @Override
     void brew() {
