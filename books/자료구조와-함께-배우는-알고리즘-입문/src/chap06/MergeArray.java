15c2a09fe3ebf90233442c410d7c71a536a3e13a
kopro-ov <koprodev@gmail.com>
2022-07-24 23:39:30 +0900

    merge sort
    병합 정렬은 배열을 앞부분과 뒷부분으로 나누어 각각 정렬한 다음
    병합하는 작업을 반복하여 정렬을 수행하는 알고리즘

---
+++ books/자료구조와-함께-배우는-알고리즘-입문/src/chap06/MergeArray.java
@@ -0,0 +1,35 @@
+package chap06;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MergeArray {
+
+    static void merge(int[] a, int na, int[] b, int nb, int[] c) {
+        int pa = 0;
+        int pb = 0;
+        int pc = 0;
+
+        while (pa < na && pb < nb)
+            c[pc++] = (a[pa] <= b[pb]) ? a[pa++] : b[pb++];
+
+        while (pa < na)
+            c[pc++] = a[pa++];
+
+        while (pb < nb)
+            c[pc++] = b[pb++];
+    }
+
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+        int[] a = {2, 4, 6, 8, 11, 13};
+        int[] b = {1, 2, 3, 4, 9, 16, 21};
+        int[] c = new int[13];
+
+        System.out.println("두 배열의 병합");
+
+        merge(a, a.length, b, b.length, c);
+
+    }
+
+}
