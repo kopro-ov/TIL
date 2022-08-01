675fb853decca004cff0ba5806016cf215797939
kopro-ov <koprodev@gmail.com>
2022-08-01 22:22:44 +0900

    동작 파라미터화 코드 전달하기
    변화하는 요구사항에 대흥
    첫 번쨰 시도 녹색 사과 필터링

---
+++ books/modern-java-in-action/ex/src/main/java/chapter02/apple/Apple.java
@@ -0,0 +1,14 @@
+package chapter02.apple;
+
+public class Apple {
+
+    Enum color;
+
+    public Apple(Enum color) {
+        this.color = color;
+    }
+
+    public Enum getColor() {
+        return color;
+    }
+}
