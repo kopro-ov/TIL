fe042c9ba4ea47ab40b86559e612cf8afe5c437c
kopro-ov <koprodev@gmail.com>
2022-08-13 23:10:51 +0900

    요소 찾기

---
+++ books/modern-java-in-action/ex/src/main/java/chapter05/SomeNumbers.java
@@ -0,0 +1,19 @@
+package chapter05;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+public class SomeNumbers {
+
+    public static void main(String[] args) {
+
+        List<Integer> someNumbers = Arrays.asList(1, 2, 3, 4, 5);
+        Optional<Integer> firstSquareDivisibleByThree =
+                someNumbers.stream()
+                        .map(n -> n * n)
+                        .filter(n -> n % 3 == 0)
+                        .findFirst();
+
+    }
+}
