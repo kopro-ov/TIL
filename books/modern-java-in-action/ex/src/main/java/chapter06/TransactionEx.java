fa5bac55069f2ebf6eaae90b65470df507a5c6c3
kopro-ov <koprodev@gmail.com>
2022-08-15 22:09:13 +0900

    Trader와 Transaction 클래스 정의 및 질문의 답

---
+++ books/modern-java-in-action/ex/src/main/java/chapter06/TransactionEx.java
@@ -0,0 +1,51 @@
+package chapter06;
+
+import domain.Trader;
+import domain.Transaction;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.Comparator.comparing;
+import static java.util.stream.Collectors.toList;
+
+public class TransactionEx {
+
+    public static void main(String[] args) {
+        Trader raoul = new Trader("Raoul", "Cambridge");
+        Trader mario = new Trader("Mario", "Milan");
+        Trader alan = new Trader("Alan", "Cambridge");
+        Trader brian = new Trader("Brian", "Cambridge");
+
+        List<Transaction> transactionList = Arrays.asList(
+                new Transaction(brian, 2011, 300),
+                new Transaction(mario, 2011, 400),
+                new Transaction(brian, 2013, 710),
+                new Transaction(alan, 2014, 700),
+                new Transaction(brian, 2015, 950)
+        );
+
+        List<Transaction> tr2011 = transactionList
+                .stream()
+                .filter(transaction -> transaction.getYear() == 2011)
+                .sorted(comparing(Transaction::getValue))
+                .collect(toList());
+
+        List<String> cities = transactionList
+                .stream()
+                .map(transaction -> transaction.getTrader().getCity())
+                .distinct()
+                .collect(toList());
+
+        List<Trader> traders = transactionList
+                .stream()
+                .map(Transaction::getTrader)
+                .filter(trader -> trader.getCity().equals("Cambridge"))
+                .distinct()
+                .sorted(comparing(Trader::getName))
+                .collect(toList());
+
+    }
+
+}
