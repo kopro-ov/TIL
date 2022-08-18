7c3c131ccb5f46a2f8bbffc2e647fd2a620b4dd1
kopro-ov <koprodev@gmail.com>
2022-08-18 23:15:45 +0900

    collect와 reduce
    위 코드에는 의미론적인 문제와 실용성 문제 등 두 가지 문제가 발생한다

---
+++ books/modern-java-in-action/ex/src/main/java/chapter06/CollectAndReduce.java
@@ -0,0 +1,25 @@
+package chapter06;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class CollectAndReduce {
+
+    public static void main(String[] args) {
+
+        Stream<Integer> stream = Arrays.asList(1, 2, 3, 4, 5, 6).stream();
+        List<Integer> numbers = stream.reduce(
+                new ArrayList<Integer>(),
+                (List<Integer> l, Integer e) -> {
+                    l.add(e);
+                    return l;
+                },
+                (List<Integer> l1, List<Integer> l2) -> {
+                    l1.addAll(l2);
+                    return l1;
+                });
+
+    }
+}
